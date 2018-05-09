9324c2b0ebe93bb2f700195aea6bf14bcf4bd8a1
LEARAX57H <willculhane3@gmail.com>
2018-05-08 22:55:32 -0600

    Board gen

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
+extern crate rand;
 extern crate termion;
-
-use std::io::{Read, Write};
 
 type Tile = u64;
 
@@ -13,16 +12,24 @@ impl Game {
     pub fn get_text_board(&self) -> std::string::String {
         let mut finished_string = String::new();
         for row in self.board.iter() {
-            finished_string = finished_string + &"+---+---+---+---+".to_string();
+            finished_string = finished_string + &"+---+---+---+---+\n".to_string();
             for cell in row.iter() {
-                finished_string = finished_string + &cell.to_string();
+                finished_string = finished_string + "| " + &cell.to_string() + " ";
             }
+            finished_string = finished_string + "|\n"
         }
+        finished_string = finished_string + &"+---+---+---+---+\n".to_string();
         return finished_string;
     }
     pub fn new() -> Game {
+        use rand::Rng;
+        let mut rng = rand::thread_rng();
+        let start_x = rng.gen_range(0, 4);
+        let start_y = rng.gen_range(0, 4);
+        let mut board = [[0; 4]; 4];
+        board[start_x][start_y] = 2;
         Game {
-            board: [[0; 4]; 4],
+            board: board,
             points: 0
         }
     }
@@ -30,20 +37,20 @@ impl Game {
 
 fn main() {
     let mut game_state = Game::new();
-    println!("+---+---+---+---+");
-    println!("| 2 | 0 | 2 | 0 |");
-    println!("+---+---+---+---+");
-    println!("| 2 | 0 | 2 | 0 |");
-    println!("+---+---+---+---+");
-    println!("| 0 | 2 | 8 | 0 |");
-    println!("+---+---+---+---+");
-    println!("| 4 | 2 | 0 | 0 |");
-    println!("+---+---+---+---+");
-    Game::get_text_board(&game_state);
+    println!{"{}", Game::get_text_board(&game_state)};
 }
 
 /*
  * 1. Print board
  * 2. Capture keypress
  * 3. Game logic
+ * println!("+---+---+---+---+");
+ * println!("| 2 | 0 | 2 | 0 |");
+ * println!("+---+---+---+---+");
+ * println!("| 2 | 0 | 2 | 0 |");
+ * println!("+---+---+---+---+");
+ * println!("| 0 | 2 | 8 | 0 |");
+ * println!("+---+---+---+---+");
+ * println!("| 4 | 2 | 0 | 0 |");
+ * println!("+---+---+---+---+");
  */
