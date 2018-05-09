9b1b5d1fce63fc83fc6a7a7454b0567d9a73a6ba
LEARAX57H <willculhane3@gmail.com>
2018-05-08 22:28:52 -0600

    It compiles

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,48 @@ extern crate termion;
 
 use std::io::{Read, Write};
 
-struct Tile {
-    value: u64
+type Tile = u64;
+
+struct Game {
+    board: [[Tile; 4]; 4],
+    points: u8
 }
 
-struct Game<R, W: Write> {
-    board: [[Tile; 4]; 4],
-    points: u8,
-    stdin: R,
-    stdout: W
+impl Game {
+    pub fn get_text_board(&self) -> std::string::String {
+        let mut finished_string = String::new();
+        for row in self.board.iter() {
+            finished_string = finished_string + &"+---+---+---+---+".to_string();
+            for cell in row.iter() {
+                finished_string = finished_string + &cell.to_string();
+            }
+        }
+        return finished_string;
+    }
+    pub fn new() -> Game {
+        Game {
+            board: [[0; 4]; 4],
+            points: 0
+        }
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut game_state = Game::new();
+    println!("+---+---+---+---+");
+    println!("| 2 | 0 | 2 | 0 |");
+    println!("+---+---+---+---+");
+    println!("| 2 | 0 | 2 | 0 |");
+    println!("+---+---+---+---+");
+    println!("| 0 | 2 | 8 | 0 |");
+    println!("+---+---+---+---+");
+    println!("| 4 | 2 | 0 | 0 |");
+    println!("+---+---+---+---+");
+    Game::get_text_board(&game_state);
 }
+
+/*
+ * 1. Print board
+ * 2. Capture keypress
+ * 3. Game logic
+ */
