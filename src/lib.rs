95a384dcc7e45b306d95f91f8a37d358e8e89dc6
LEARAX57H <willculhane3@gmail.com>
2018-05-11 07:18:33 -0600

    Fix East

--- src/lib.rs
+++ src/lib.rs
@@ -57,7 +57,7 @@ impl Game {
             }
             Some(Move::East) => {
                 for mut row in &mut board.iter_mut() {
-                    for column in (0..3).rev() {
+                    for column in (0..4).rev() {
                         for next_column in (0..column).rev() {
                             if row[next_column] != 0 {
                                 if row[column] == 0 as Tile {
