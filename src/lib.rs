4bce70eb36cbe4ad923c1c01d8e49539ee7a3f98
LEARAX57H <willculhane3@gmail.com>
2018-05-11 23:01:38 -0600

    Broke it

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ pub enum Move {
     South,
 }
 
+#[derive(Default)]
 pub struct Game {
     pub board: Board,
     pub score: usize,
@@ -24,113 +25,116 @@ impl Game {
         let value = if rng.gen_weighted_bool(10) { 4 } else { 2 };
         let mut zero_row = vec![];
         let mut zero_column = vec![];
-        for row in 0..4 {
-            for column in 0..4 {
-                if board[row][column] == 0 {
-                    zero_row.push(row);
-                    zero_column.push(column);
+        for (row_index, row) in board.iter().enumerate() {
+            for (column_index, tile) in row.iter().enumerate() {
+                if *tile == 0 {
+                    zero_row.push(row_index);
+                    zero_column.push(column_index);
                 }
             }
         }
-        if zero_row.len() != 0 {
+        if zero_row.is_empty() {
             let index_x = rng.gen_range(0, zero_row.len());
             let index_y = rng.gen_range(0, zero_column.len());
             board[zero_row[index_x]][zero_column[index_y]] = value;
         }
         board
     }
-    pub fn handle_move(board: &mut Board, game_move: Option<Move>) {
+    pub fn move_north(board: &mut Board) {
+        for column in 0..4 {
+            for row in (0..4).rev() {
+                for next_row in (row + 1)..4 {
+                    if board[next_row][column] != 0 {
+                        if board[row][column] == 0 as Tile {
+                            board[row][column] = board[next_row][column];
+                            board[next_row][column] = 0;
+                        } else if board[row][column] == board[next_row][column] {
+                            board[row][column] += board[row][column];
+                            board[next_row][column] = 0;
+                            break;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    pub fn move_west(board: &mut Board) {
+        for mut row in &mut board.iter_mut() {
+            for column in 0..3 {
+                for next_column in (column + 1)..4 {
+                    if row[next_column] != 0 {
+                        if row[column] == 0 as Tile {
+                            row[column] = row[next_column];
+                            row[next_column] = 0;
+                        } else if row[column] == row[next_column] {
+                            row[column] += row[column];
+                            row[next_column] = 0;
+                            break;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    pub fn move_east(board: &mut Board) {
+        for mut row in &mut board.iter_mut() {
+            for column in (0..4).rev() {
+                for next_column in (0..column).rev() {
+                    if row[next_column] != 0 {
+                        if row[column] == 0 as Tile {
+                            row[column] = row[next_column];
+                            row[next_column] = 0;
+                        } else if row[column] == row[next_column] {
+                            row[column] += row[column];
+                            row[next_column] = 0;
+                            break;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    pub fn move_south(board: &mut Board) {
+        for column in 0..4 {
+            for row in (0..4).rev() {
+                for next_row in (0..row).rev() {
+                    if board[next_row][column] != 0 {
+                        if board[row][column] == 0 as Tile {
+                            board[row][column] = board[next_row][column];
+                            board[next_row][column] = 0;
+                        } else if board[row][column] == board[next_row][column] {
+                            board[row][column] += board[row][column];
+                            board[next_row][column] = 0;
+                            break;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    pub fn handle_move(board: &mut Board, game_move: &Move) {
         match game_move {
-            Some(Move::North) => {
-                for column in 0..4 {
-                    for row in (0..4).rev() {
-                        for next_row in (row + 1)..4 {
-                            if board[next_row][column] != 0 {
-                                if board[row][column] == 0 as Tile {
-                                    board[row][column] = board[next_row][column];
-                                    board[next_row][column] = 0;
-                                } else if board[row][column] == board[next_row][column] {
-                                    board[row][column] += board[row][column];
-                                    board[next_row][column] = 0;
-                                    break;
-                                } else {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            Some(Move::West) => {
-                for mut row in &mut board.iter_mut() {
-                    for column in 0..3 {
-                        for next_column in (column + 1)..4 {
-                            if row[next_column] != 0 {
-                                if row[column] == 0 as Tile {
-                                    row[column] = row[next_column];
-                                    row[next_column] = 0;
-                                } else if row[column] == row[next_column] {
-                                    row[column] += row[column];
-                                    row[next_column] = 0;
-                                    break;
-                                } else {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            Some(Move::East) => {
-                for mut row in &mut board.iter_mut() {
-                    for column in (0..4).rev() {
-                        for next_column in (0..column).rev() {
-                            if row[next_column] != 0 {
-                                if row[column] == 0 as Tile {
-                                    row[column] = row[next_column];
-                                    row[next_column] = 0;
-                                } else if row[column] == row[next_column] {
-                                    row[column] += row[column];
-                                    row[next_column] = 0;
-                                    break;
-                                } else {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            Some(Move::South) => {
-                for column in 0..4 {
-                    for row in (0..4).rev() {
-                        for next_row in (0..row).rev() {
-                            if board[next_row][column] != 0 {
-                                if board[row][column] == 0 as Tile {
-                                    board[row][column] = board[next_row][column];
-                                    board[next_row][column] = 0;
-                                } else if board[row][column] == board[next_row][column] {
-                                    board[row][column] += board[row][column];
-                                    board[next_row][column] = 0;
-                                    break;
-                                } else {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            None => {}
+            Move::North => Game::move_north(board),
+            Move::West => Game::move_west(board),
+            Move::East => Game::move_east(board),
+            Move::South => Game::move_south(board)
         }
     }
     pub fn get_text_board(board: Board) -> std::string::String {
         let mut max_digits = [0; 4];
-        for column in 0..4 {
-            for row in 0..4 {
-                let digits = digits(board[row][column]);
-                if digits > max_digits[column] {
-                    max_digits[column] = digits
+        for row in board.iter() {
+            for (col_index, tile) in row.iter().enumerate() {
+                let digits = digits(tile);
+                if digits > max_digits[col_index] {
+                    max_digits[col_index] = digits
                 }
             }
         }
@@ -147,30 +151,26 @@ impl Game {
         );
 
         let mut finished_string = String::new();
-        for row in &board {
+        for row in board.iter() {
             finished_string = finished_string + &divider;
-            for column in 0..4 {
-                let padding_spaces = " ".repeat(1 + max_digits[column] - digits(row[column]));
+            for (col_index, column) in row.iter().enumerate() {
+                let padding_spaces = " ".repeat(1 + max_digits[col_index] - digits(&row[col_index]));
                 finished_string =
-                    finished_string + "| " + &row[column].to_string() + &padding_spaces;
+                    finished_string + "| " + &row[col_index].to_string() + &padding_spaces;
             }
             finished_string += "|\r\n"
         }
-        finished_string = finished_string + &divider;
-        return finished_string;
+        finished_string + &divider
     }
     pub fn new() -> Game {
         let board = [[0; 4]; 4];
-        Game {
-            board,
-            score: 0,
-        }
+        Game { board, score: 0 }
     }
 }
 
-fn digits(number: usize) -> usize {
-    if number != 0 {
-        (number as f32).log10().ceil() as usize
+fn digits(number: &usize) -> usize {
+    if *number != 0 {
+        (*number as f32).log10().ceil() as usize
     } else {
         1
     }
