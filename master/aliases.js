5114746fa5aac567d1485e756b413a7e078e7e59
Travis CI User <travis@example.org>
2018-07-27 18:12:43 +0000

    Automatic Travis documentation build

--- master/aliases.js
+++ master/aliases.js
@@ -1,7 +1,12 @@
 var ALIASES = {};
 ALIASES['jocors'] = {};
+ALIASES['jocors'] = {};
 ALIASES['libc'] = {};
 ALIASES['pack_2048'] = {};
+ALIASES['pack_2048'] = {};
+ALIASES['ptf8'] = {};
 ALIASES['ptf8'] = {};
 ALIASES['rand'] = {};
+ALIASES['rand'] = {};
+ALIASES['rand_core'] = {};
 ALIASES['termion'] = {};
