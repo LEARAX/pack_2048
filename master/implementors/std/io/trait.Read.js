7fd534006380a75234d85dbfe34bd17e0cb31c46
Travis CI User <travis@example.org>
2018-07-27 18:13:28 +0000

    Automatic Travis documentation build

--- master/implementors/std/io/trait.Read.js
+++ master/implementors/std/io/trait.Read.js
@@ -1,5 +1,4 @@
 (function() {var implementors = {};
-implementors["rand_core"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"trait\" href=\"rand_core/trait.RngCore.html\" title=\"trait rand_core::RngCore\">RngCore</a>",synthetic:false,types:["rand_core::RngCore"]},];
 implementors["termion"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"termion/struct.AsyncReader.html\" title=\"struct termion::AsyncReader\">AsyncReader</a>",synthetic:false,types:["termion::async::AsyncReader"]},];
 
             if (window.register_implementors) {
